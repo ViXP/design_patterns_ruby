273469c8783fd845dbf19baed03dd0f3e536394b
Cyril ViXP <cyrilvixp@gmail.com>
2017-11-02 20:22:03 +0200

    Proxy pattern is implemented

---
+++ proxy/proxy.rb
@@ -0,0 +1,27 @@
+# CONCRETE PROXY
+require './subject'
+
+class InvestorsRegisterProxy < AbstractInvestorsRegister
+  def initialize *args
+    super *args
+    @subject = false
+  end
+
+  def show_register
+    return @subject.show_register if @subject
+    stringify_info({full_name: full_name, company: company, amount: amount}) + 
+    "\e[32mTHE ORIGINAL REGISTER WILL BE ACCESSIBLE AFTER THE INVESTING\e[0m"
+  end
+
+  def invest!
+    return puts 'Already invested, thank you!' if @subject
+    puts "Are you really wish to invest #{amount}$?"
+    if gets.include? 'y'
+      puts 'SAVING...'
+      @subject = InvestorsRegister.new(full_name, company, amount)
+      @subject.invest!
+      puts 'DATA IS SAVED!'
+      @subject
+    end
+  end
+end
---
+++ proxy/subject.rb
@@ -0,0 +1,66 @@
+# ABSTRACT SUBJECT
+class AbstractInvestorsRegister
+  attr_reader :full_name, :company, :amount
+
+  def initialize full_name, company, amount
+    @full_name = full_name
+    @company = company
+    @amount = amount
+  end
+
+  def invest!
+    raise NotImplementedError, 'Please, redeclare this method in a child class, don\'t use the abstract method!'
+  end
+
+  def show_register
+    raise NotImplementedError, 'Please, redeclare this method in a child class, don\'t use the abstract method!'
+  end
+  
+  private
+
+  def stringify_info investor
+    <<~TEXT
+      Full investor name: #{investor[:full_name]}
+      Company name: #{investor[:company]}
+      Amount of money invested: #{investor[:amount]}$\n
+    TEXT
+  end
+end
+
+# CONCRETE SUBJECT
+class InvestorsRegister < AbstractInvestorsRegister
+  def initialize *args
+    super *args
+    @data_file = './data'
+    @register = []
+  end
+
+  def invest!
+    new_info = {full_name: full_name, company: company, amount: amount}
+    load_data
+    @register << new_info
+    File.open(@data_file, 'w') {|file| file.write(Marshal.dump(@register)) }
+  end
+
+  def show_register
+    full_info = ''
+    load_data
+    @register.each {|investor| full_info += stringify_info(investor) }
+    full_info
+  end
+
+  def clear_data!
+    puts 'Are you sure you wish to fully clear the register?'
+    if gets.include? 'y'
+      File.open(@data_file, 'w') {|file| file.write('') }
+      load_data
+    end
+  end
+
+  private 
+
+  def load_data
+    object = File.read(@data_file)
+    @register = object == '' ? [] : Marshal.load(object)
+  end
+end

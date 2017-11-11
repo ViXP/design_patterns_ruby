3c3317267a1e3a7b911deb8e2142e3072b99d061
Cyril ViXP <cyrilvixp@gmail.com>
2017-11-11 14:37:25 +0200

    Bridge modified (syntax style is fixed)

--- bridge/abstractions.rb
+++ bridge/abstractions.rb
@@ -6,7 +6,7 @@ class Entity
   attr_reader :type, :move_type
   attr_accessor :room
 
-  def initialize room = Room.new
+  def initialize(room = Room.new)
     @room = room
   end
 
@@ -21,7 +21,7 @@ end
 class Robot < Entity
   attr_reader :model
 
-  def initialize room, model
+  def initialize(room, model)
     super room
     @model = model
     @move_type = :drive_in
@@ -29,7 +29,9 @@ class Robot < Entity
   end
 
   def action
-    puts terminal_add("SECTOR: #{room.label.upcase}, PURPOSE: #{room.type.upcase}")
+    puts terminal_add(
+      "SECTOR: #{room.label.upcase}, PURPOSE: #{room.type.upcase}"
+    )
     analyze
     find_objects
     puts terminal_add(room.move_in(move_type).upcase)
@@ -46,13 +48,13 @@ class Robot < Entity
     puts terminal_add(room.analyze.upcase)
   end
 
-  def hibernate! seconds
+  def hibernate!(seconds)
     puts terminal_add("GOING TO HIBERNATE MY SYSTEMS IN #{seconds} SECONDS")
     sleep seconds
-    puts terminal_add("HIBERNATION...")
+    puts terminal_add('HIBERNATION...')
   end
 
-  def terminal_add text
+  def terminal_add(text)
     "#{DateTime.now} -- #{text.upcase}"
   end
 end
@@ -60,7 +62,7 @@ end
 class PlayfulKid < Entity
   attr_reader :first_name, :height
 
-  def initialize room, f_name, height = 135
+  def initialize(room, f_name, height = 135)
     super room
     @first_name = f_name
     @height = height
@@ -78,25 +80,25 @@ class PlayfulKid < Entity
     puts 'There are a lot of things to play with!'
   end
 
-  private 
+  private
 
   def analyze
     if rand(1..2) == 1
-      "I see castles, lava and dragons!"
+      'I see castles, lava and dragons!'
     else
-      "I see knights, kings and queens!"
+      'I see knights, kings and queens!'
     end
   end
 
   def play
-    "I've already started playing"
+    'I\'ve already started playing'
   end
 end
 
 class CarefulMan < Entity
   attr_reader :full_name
 
-  def initialize room, full_name
+  def initialize(room, full_name)
     super room
     @move_type = :slow_steps
     @full_name = full_name
@@ -121,6 +123,7 @@ class CarefulMan < Entity
   end
 
   def introduce
-    puts "My name is Mr.#{full_name} and I shall not do anything, until I shall be sure what I see. So..."
+    puts "My name is Mr.#{full_name} and I shall not do anything, until I " \
+    'shall be sure what I see. So...'
   end
 end
--- bridge/implementors.rb
+++ bridge/implementors.rb
@@ -2,15 +2,18 @@
 class Room
   attr_reader :label, :area, :type
 
-  def initialize label, area
+  def initialize(label, area)
     @label = label
-    @area = area    
+    @area = area
     @type = self.class.name
     @objects = []
   end
 
   def analyze
-    raise NotImplementedError, 'Please, redeclare this method in a child class, don\'t use the abstract method!'
+    raise(
+      NotImplementedError, \
+      'Redeclare this method in a child class, don\'t use the abstract method!'
+    )
   end
 
   def objects
@@ -19,50 +22,56 @@ class Room
     string
   end
 
-  def move_in m_type
+  def move_in(m_type)
     "Moving-in forward to the room by #{m_type}"
   end
 end
 
 # CONCRETE IMPLEMENTORS
 class BedRoom < Room
-  def initialize *args
-    super *args
+  def initialize(*args)
+    super(*args)
     @type = 'Bed room for sleeping and relaxing'
-    @objects = [:lamp, :sofa, :TV, :bed, :chairs, :commode]
+    @objects = %i[lamp sofa TV bed chairs commode]
   end
 
   def analyze
-    @area > 20 ? "The comfortable and cozy room, area size is #{area} square meters" : "Small and uncomfortable room"
+    if @area > 20
+      "The comfortable and cozy room, area size is #{area} square meters"
+    else
+      'Small and uncomfortable room'
+    end
   end
 
-  def move_in m_type
+  def move_in(m_type)
     "Quietly moving-in forward to the room by #{m_type}"
   end
 end
 
 class BathRoom < Room
-  def initialize *args
-    super *args
-    @objects = [:bath, :soap, :towels, :shampoo, :shelves, :mirror, :dispenser]
+  def initialize(*args)
+    super(*args)
+    @objects = %i[bath soap towels shampoo shelves mirror dispenser]
   end
 
   def analyze
-    "The bathroom with colorful tiles on the walls (the area size is #{area} square meters)"
+    "The bathroom with colorful tiles on the walls (the area size is #{area} " \
+    'square meters)'
   end
 
-  def move_in m_type
+  def move_in(m_type)
     "Carefully moving-in forward to the room with slippery floor by #{m_type}"
   end
 end
 
 class DiningRoom < Room
-  def initialize *args
-    super *args
-    @objects = [:table, :dishwasher, :refrigerator, :chairs, :kettle, :blender]
+  def initialize(*args)
+    super(*args)
+    @objects = %i[table dishwasher refrigerator chairs kettle blender]
   end
 
   def analyze
-    "The full kitchen area is #{area} square meters, #{(area/3).ceil} men can eat here"
+    "The full kitchen area is #{area} square meters, #{(area / 3).ceil} men " \
+    'can eat here'
   end
 end
